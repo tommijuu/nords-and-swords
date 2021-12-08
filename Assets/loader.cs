74d7dfd0b501d8cc8ebac05e8e03fd8849ca158e
Tommi Juutilainen <juutilainen.tommi@gmail.com>
2021-12-08 19:13:23 +0200

    Adding an old school project to git for demo purposes

---
+++ Assets/loader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void MErchant()
+    {
+        PlayerAttributes save = new PlayerAttributes();
+        save.SavePlayer(); // Attribuuttien tallennus ennen merchant-sceneä
+        SceneManager.LoadScene("MapScene");
+
+    }
+}
